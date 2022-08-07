b026d6b495caf1f1f0657e3294a1d86381ee5dae
Robson Gomes <nosbor84@gmail.com>
2022-08-07 08:25:23 -0300

    parte 09

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
@@ -12,15 +13,81 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/robsongomes/service/app/business/data/schema"
+	"github.com/robsongomes/service/app/business/sys/database"
 )
 
 func main() {
-	err := getToken()
+	err := Migrate()
 
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	err = Seed()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// Migrate creates the schema in the database.
+func Migrate() error {
+	cfg := database.Config{
+		User:         "postgres",
+		Password:     "postgres",
+		Host:         "localhost",
+		Name:         "postgres",
+		MaxIdleConns: 0,
+		MaxOpenConns: 0,
+		DisableTLS:   true,
+	}
+
+	db, err := database.Open(cfg)
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := schema.Migrate(ctx, db); err != nil {
+		return fmt.Errorf("migrate database: %w", err)
+	}
+
+	fmt.Println("migrations complete")
+	return nil
+}
+
+// Seed loads test data into the database.
+func Seed() error {
+	cfg := database.Config{
+		User:         "postgres",
+		Password:     "postgres",
+		Host:         "localhost",
+		Name:         "postgres",
+		MaxIdleConns: 0,
+		MaxOpenConns: 0,
+		DisableTLS:   true,
+	}
+	db, err := database.Open(cfg)
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := schema.Seed(ctx, db); err != nil {
+		return fmt.Errorf("seed database: %w", err)
+	}
+
+	fmt.Println("seed data complete")
+	return nil
 }
 
 func getToken() error {
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.18
 
 require (
 	github.com/ardanlabs/conf/v3 v3.1.2
+	github.com/ardanlabs/darwin v1.3.0
+	github.com/ardanlabs/service v0.0.0-20220728190559-134d9d429b79
 	github.com/dimfeld/httptreemux/v5 v5.4.0
 	github.com/golang-jwt/jwt/v4 v4.4.2
 	github.com/google/uuid v1.3.0
@@ -14,6 +16,13 @@ require (
 )
 
 require (
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.33.0 // indirect
+	go.opentelemetry.io/otel v1.8.0 // indirect
+	go.opentelemetry.io/otel/metric v0.31.0 // indirect
+	go.opentelemetry.io/otel/trace v1.8.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
 )
